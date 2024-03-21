e4a379439743cbf6dba723098076e8da7e48dfdb
doeyeon <93880782+doeyeon@users.noreply.github.com>
2024-03-20 20:30:12 -0400

    update index.tsx
    Navbar headers, font styling, hover functions, button replacement, and card styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,23 @@ import { motion, MotionStyle, useAnimation } from "framer-motion";
 import Image from 'next/image';
 import Head from 'next/head';
 
-const TopNavProfiles = styled(motion.div)`
+const TopNavLinkedin = styled(motion.div)`
   display: flex;
   justify-content: flex-end;
   padding: 20px;
-  margin-right: 70px;
+  margin-right: 10vw;
+  margin-top: 20px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 100;
+`;
+
+const TopNavEmail = styled(motion.div)`
+  display: flex;
+  justify-content: flex-end;
+  padding: 20px;
+  margin-right: 5vw;
   margin-top: 20px;
   position: absolute;
   top: 0;
@@ -55,7 +67,7 @@ const CenteredSection = styled.div`
 const ExperienceSection = styled.section`
   display: grid;
   grid-template-columns: repeat(2, minmax(0, max-content));
-  gap: 10vh 6vw;
+  gap: 10vh 5vw;
   justify-content: center;
   align-items: start;
   padding: 20px;
@@ -88,23 +100,15 @@ const ScrollContainer = styled.div`
     width: 10px;
   }
   &::-webkit-scrollbar-track {
-    background: #f8f4f9; 
+    background: #e9ecef; 
   }
   &::-webkit-scrollbar-thumb {
-    background: #e0ded3;
+    background: #4361ee;
     border-radius: 10px;
   }
   &::-webkit-scrollbar-thumb:hover {
     background: #9a8c98;
   }
-  scrollbar-face-color: #e0ded3;
-  scrollbar-base-color: #f8f4f9;
-  scrollbar-3dlight-color: #f8f4f9;
-  scrollbar-highlight-color: #f8f4f9;
-  scrollbar-track-color: #f8f4f9;
-  scrollbar-arrow-color: #e0ded3;
-  scrollbar-shadow-color: #f8f4f9;
-  scrollbar-dark-shadow-color: #f8f4f9;
 `;
 
 const ButtonContainer = styled.div`
@@ -125,12 +129,10 @@ const GifContainer = styled.div`
   position: relative;
 `;
 
-const fontFamilyStyle = { fontFamily: "'Biotif', 'Segoe UI', 'Roboto', 'Arial', sans-serif" };
-
 const cardVariants = {
   closed: {
-    width: "18vh",
-    height: "18vh",
+    width: "19vh",
+    height: "19vh",
     borderRadius: "50%",
     opacity: 1,
     transition: {
@@ -141,9 +143,9 @@ const cardVariants = {
     }
   },
   open: {
-    width: "40vw",
+    width: "41vw",
     height: "35vh",
-    borderRadius: "15px",
+    borderRadius: "12px",
     opacity: 1,
     transition: {
       duration: 1,
@@ -163,20 +165,20 @@ const cardVariants = {
 function IntroductionSection() {
   return (
     <CenteredSection>
-      <h2 style={{ ...fontFamilyStyle, fontWeight: 800, fontSize: 40, textAlign: 'center' }}>Hi, I&apos;m Tony</h2>
-      <h3 style={{ ...fontFamilyStyle, fontWeight: 400, fontSize: 16, marginBottom: 40, textAlign: 'center' }}>B.S (Hons) Computer Science & Math Minor</h3>
-      <h3 style={{ ...fontFamilyStyle, fontWeight: 400, fontSize: 24, marginBottom: 20, textAlign: 'center' }}>
+      <h2 style={{ fontWeight: 600, fontSize: 56, textAlign: 'center' }}>Hi, I&apos;m Tony</h2>
+      <h3 style={{ fontWeight: 400, fontSize: 22, marginBottom: 40, textAlign: 'center', color: '#495057' }}>B.S (Hons) Computer Science & Math Minor</h3>
+      <h3 style={{ fontWeight: 400, fontSize: 24, marginBottom: 20, textAlign: 'center' }}>
         Currently seeking full-time roles
       </h3>
-      <h3 style={{ ...fontFamilyStyle, fontWeight: 400, fontSize: 24, textAlign: 'center' }}>
+      <h3 style={{ fontWeight: 400, fontSize: 24, textAlign: 'center' }}>
         Previously @
-        <a href="https://www.iata.org/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: 800, color: '#4361ee' }}> IATA</a>,
-        <a href="https://www.gm.com" target="_blank" rel="noopener noreferrer" style={{ fontWeight: 800, color: '#4361ee' }}> General Motors</a>,
-        <a href="https://www.birdandbe.com" target="_blank" rel="noopener noreferrer" style={{ fontWeight: 800, color: '#4361ee' }}> Bird&Be</a>
+        <a href="https://www.iata.org/" target="_blank" rel="noopener noreferrer" style={{ fontWeight: 800, color: '#072ac8' }}> IATA</a>,
+        <a href="https://www.gm.com" target="_blank" rel="noopener noreferrer" style={{ fontWeight: 800, color: '#1e96fc' }}> General Motors</a>,
+        <a href="https://www.birdandbe.com" target="_blank" rel="noopener noreferrer" style={{ fontWeight: 800, color: '#7400b8' }}> Bird&Be</a>
       </h3>
-      <h3 style={{ ...fontFamilyStyle, fontWeight: 400, fontSize: 24, textAlign: 'center' }}>
+      <h3 style={{ fontWeight: 400, fontSize: 24, textAlign: 'center' }}>
         and Co-founder of
-        <a href="https://www.phaserx.ca" target="_blank" rel="noopener noreferrer" style={{ fontWeight: 800, color: '#4361ee' }}> PhaseRx</a>
+        <a href="https://www.phaserx.ca" target="_blank" rel="noopener noreferrer" style={{ fontWeight: 800, color: '#ef476f' }}> PhaseRx</a>
       </h3>
     </CenteredSection >
   );
@@ -246,31 +248,31 @@ function ExperienceCard({ experience }: { experience: ProfessionalExperience }) 
         >
           <ScrollContainer style={{ overflowY: isOpen ? 'auto' : 'hidden' }}>
 
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 600, fontSize: 20, marginBottom: 5 }}>
+            <h3 style={{ fontWeight: 600, fontSize: 18, marginBottom: 5 }}>
               <span style={{ fontWeight: 'bold', color: '#4361ee' }}>Company: </span>
               {experience.company}
             </h3>
 
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 600, fontSize: 20, marginBottom: 5 }}>
+            <h3 style={{ fontWeight: 600, fontSize: 18, marginBottom: 5 }}>
               <span style={{ fontWeight: 'bold', color: '#4361ee' }}>Role: </span>
               {experience.role}
             </h3>
 
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 600, fontSize: 20, marginBottom: 5 }}>
+            <h3 style={{ fontWeight: 600, fontSize: 18, marginBottom: 5 }}>
               <span style={{ fontWeight: 'bold', color: '#4361ee' }}>Year: </span>
               {experience.year}
             </h3>
 
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 600, fontSize: 20, marginBottom: 20 }}>
+            <h3 style={{ fontWeight: 600, fontSize: 18, marginBottom: 20 }}>
               <span style={{ fontWeight: 'bold', color: '#4361ee' }}>Key Skills: </span>
               {experience.skills}
             </h3>
 
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 'bold', fontSize: 24, color: '#4361ee', marginBottom: 10 }}>
+            <h3 style={{ fontWeight: 'bold', fontSize: 18, color: '#4361ee', marginBottom: 10 }}>
               {experience.summary_title}
             </h3>
 
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 400, fontSize: 20, marginBottom: 20 }}>
+            <h3 style={{ fontWeight: 400, fontSize: 18, marginBottom: 20 }}>
               {experience.summary_paragraph}
             </h3>
             {
@@ -288,30 +290,30 @@ function ExperienceCard({ experience }: { experience: ProfessionalExperience }) 
                 </GifContainer>
               )
             }
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 'bold', fontSize: 24, color: '#4361ee', marginBottom: 10 }}>
+            <h3 style={{ fontWeight: 'bold', fontSize: 22, color: '#4361ee', marginBottom: 10 }}>
               {experience.background_title}
             </h3>
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 500, fontSize: 22, color: 'black', marginBottom: 10 }}>
+            <h3 style={{ fontWeight: 500, fontSize: 20, color: 'black', marginBottom: 10 }}>
               {experience.background_subtitle}
             </h3>
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 400, fontSize: 20, marginBottom: 10 }}>
+            <h3 style={{ fontWeight: 400, fontSize: 18, marginBottom: 10 }}>
               {experience.background_paragraph}
             </h3>
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 500, fontSize: 22, color: 'black', marginBottom: 10 }}>
+            <h3 style={{ fontWeight: 500, fontSize: 20, color: 'black', marginBottom: 10 }}>
               {experience.about_title}
             </h3>
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 400, fontSize: 20, marginBottom: 10 }}>
+            <h3 style={{ fontWeight: 400, fontSize: 18, marginBottom: 10 }}>
               {experience.about_paragraph}
             </h3>
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 'bold', fontSize: 24, color: '#4361ee', marginBottom: 10, marginTop: 10 }}>
+            <h3 style={{ fontWeight: 'bold', fontSize: 22, color: '#4361ee', marginBottom: 10, marginTop: 10 }}>
               {experience.project_title}
             </h3>
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 400, fontSize: 20, marginBottom: 10 }}>
+            <h3 style={{ fontWeight: 400, fontSize: 18, marginBottom: 10 }}>
               {experience.project_summary}
             </h3>
             {
               experience.company !== 'PhaseRx' && (
-                <ImageContainer style={{ marginBottom: 20, marginTop: 20 }}>
+                <ImageContainer style={{ marginBottom: 18, marginTop: 20 }}>
                   <Image
                     src={`/diagrams/${experience.diagram1}`}
                     alt={`${experience.company} diagram1`}
@@ -322,16 +324,16 @@ function ExperienceCard({ experience }: { experience: ProfessionalExperience }) 
                 </ImageContainer>
               )
             }
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 'bold', fontSize: 20, color: '#4361ee', marginBottom: 10, marginTop: 10 }}>
+            <h3 style={{ fontWeight: 'bold', fontSize: 18, color: '#4361ee', marginBottom: 10, marginTop: 10 }}>
               {experience.project_subheading1}
             </h3>
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 400, fontSize: 20, marginBottom: 10 }}>
+            <h3 style={{ fontWeight: 400, fontSize: 18, marginBottom: 10 }}>
               {experience.project_desc1}
             </h3>
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 'bold', fontSize: 20, color: '#4361ee', marginBottom: 10 }}>
+            <h3 style={{ fontWeight: 'bold', fontSize: 18, color: '#4361ee', marginBottom: 10 }}>
               {experience.project_subheading2}
             </h3>
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 400, fontSize: 20, marginBottom: 10 }}>
+            <h3 style={{ fontWeight: 400, fontSize: 18, marginBottom: 10 }}>
               {experience.project_desc2}
             </h3>
             {
@@ -347,10 +349,10 @@ function ExperienceCard({ experience }: { experience: ProfessionalExperience }) 
                 </ImageContainer>
               )
             }
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 'bold', fontSize: 20, color: '#4361ee', marginBottom: 10 }}>
+            <h3 style={{ fontWeight: 'bold', fontSize: 18, color: '#4361ee', marginBottom: 10 }}>
               {experience.project_subheading3}
             </h3>
-            <h3 style={{ ...fontFamilyStyle, fontWeight: 400, fontSize: 20, marginBottom: 10 }}>
+            <h3 style={{ fontWeight: 400, fontSize: 18, marginBottom: 10 }}>
               {experience.project_desc3}
             </h3>
           </ScrollContainer>
@@ -403,12 +405,7 @@ function GetProfessionalExperience({ experience }: { experience: ProfessionalExp
             style={{ cursor: 'pointer', display: 'inline-block' }}
             onClick={toggleExperienceVisibility}
           >
-            <Image
-              src="/buttons/Button1.svg"
-              alt="Load Experience"
-              width={330}
-              height={70}
-            />
+            <h1 style={{ fontWeight: 400, fontSize: 24, textAlign: 'center' }}>Click here to discover my work ↓</h1>
           </motion.div>
         </ButtonContainer>
       )}
@@ -426,8 +423,6 @@ function GetProfessionalExperience({ experience }: { experience: ProfessionalExp
 }
 
 function ProfessionalExperienceDisplay({ experience }: { experience: ProfessionalExperience[] }) {
-  console.log(experience);
-  console.log('hi')
   return (
     <>
       <Head>
@@ -449,17 +444,26 @@ function ProfessionalExperienceDisplay({ experience }: { experience: Professiona
         variants={navVariants}
         transition={{ duration: 0.5 }}
       >
-        <a style={{ ...fontFamilyStyle, fontWeight: 500, fontSize: 16, color: 'black', textAlign: 'center' }} href="https://github.com/doeyeon/tony-profile/blob/main/pages/index.tsx" target="_blank" rel="noopener noreferrer">View codebase for this site<br />(link to my github)</a>
+        <a style={{ fontWeight: 500, fontSize: 16, color: 'black', textAlign: 'center' }} href="https://github.com/doeyeon/tony-profile/blob/main/pages/index.tsx" target="_blank" rel="noopener noreferrer">View codebase for this site<br />(link to my github)</a>
       </TopNavCodebase >
-      <TopNavProfiles
+      <TopNavLinkedin
         initial="hidden"
         animate="visible"
+        whileHover="hover"
         variants={navVariants}
         transition={{ duration: 0.5 }}
       >
-        <a style={{ ...fontFamilyStyle, fontWeight: 500, fontSize: 20, color: 'black', marginRight: 30 }} href="https://www.linkedin.com/in/tony-yoon" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-        <a style={{ ...fontFamilyStyle, fontWeight: 500, fontSize: 20, color: 'black' }} href="mailto:yoon.tony@outlook.com">Email</a>
-      </TopNavProfiles >
+        <a style={{ fontWeight: 500, fontSize: 20, color: 'black', marginRight: 30 }} href="https://www.linkedin.com/in/tony-yoon" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      </TopNavLinkedin >
+      <TopNavEmail
+        initial="hidden"
+        animate="visible"
+        whileHover="hover"
+        variants={navVariants}
+        transition={{ duration: 0.5 }}
+      >
+        <a style={{ fontWeight: 500, fontSize: 20, color: 'black' }} href="mailto:yoon.tony@outlook.com">Email</a>
+      </TopNavEmail >
       <IntroductionSection></IntroductionSection>
       <GetProfessionalExperience experience={experience} />
     </>
